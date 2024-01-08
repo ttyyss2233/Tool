7f1564275bdd8978aab946c0c470fb16ae3255f9
ttyyss <133775030+ttyyss2233@users.noreply.github.com>
2024-01-08 22:23:46 +0800

    Update gdsj.js

--- Rewrite/gdsj.js
+++ Rewrite/gdsj.js
@@ -4,7 +4,7 @@
 
 [rewrite_local]
 
-^https:\/\/qiye\.gaoding\.com\/api\/(structure\/company\/current|ccm\/editors\/risk_materials) url script-response-body gdsj.js
+^https:\/\/qiye\.gaoding\.com\/api\/(structure\/company\/current|ccm\/editors\/risk_materials) url script-response-body https://raw.githubusercontent.com/ttyyss2233/Tool/main/Rewrite/gdsj.js
 
 
 [mitm]
